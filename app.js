435f6c3171f27743a5eade9222561f0e2822ca75
james-bie <james-bie@james-bies-MacBook-Pro.local>
2018-07-01 13:13:56 -0700

    Add most other directories, User Authentication, Header and footer, skeletal home page and landing page

--- app.js
+++ app.js
@@ -1,28 +1,64 @@
-var express		=require("express"),
-	app			= express(),
-	bodyParser	=require("body-parser"),
-	flash		=require("connect-flash"),
-	mongoose	=require("mongoose"),
-	passport	=require("passport"),
-	LocalStrategy = require("passport-local"),
-	methodOverride = require("method-override"),
-	Workout		=require("./models/workout"),
+var express			=require("express"),
+	app				= express(),
+	bodyParser		=require("body-parser"),
+	flash			=require("connect-flash"),
+	mongoose		=require("mongoose"),
+	passport		=require("passport"),
+	LocalStrategy 	= require("passport-local"),
+	methodOverride 	= require("method-override"),
+	Workout			=require("./models/workout"),
 	WrktComment		=require("./models/workoutcomment"),
-	User		=require("./models/user")
+	User			=require("./models/user")
 
 
 
-	app.use(bodyParser.urlencoded({extended:true}));
-	app.set("view engine", "ejs");
-	app.use(express.static(__dirname + "/public"));
-	app.use(methodOverride("_method"));
-	app.use(flash());
+//routes
+var indexRoutes		= require ("./routes/index")
+
+mongoose.connect("mongodb://localhost/crossfit");
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
 
 
-	app.listen(process.env.PORT || 3000, process.env.IP,function(){
 
-		console.log("server started");
-	})
+
+
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Once again Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+   res.locals.currentUser = req.user; //this passes the user to everyone
+   res.locals.error = req.flash("error"); //if theres anythign in flash we'll have access to it in message
+   res.locals.success = req.flash("success");
+   next();
+});
+
+app.use("/", indexRoutes);
+
+
+
+
+
+app.listen(process.env.PORT || 3000, process.env.IP,function(){
+
+	console.log("server started");
+})
+
+
 
 
 
---
+++ routes/index.js
@@ -0,0 +1,87 @@
+var express		= require("express");
+var router		= express.Router();
+var passport	= require("passport");
+var User		= require("../models/user");
+
+router.get("/", function(req,res){
+
+	res.render("landing");
+
+});
+
+//++++++++++++++++++++++++++++++++++++++++++++
+//+				Comment Routes				 +
+//++++++++++++++++++++++++++++++++++++++++++++
+
+router.get("/homepage",function(req,res){
+
+	res.render("homepage");
+});
+
+//++++++++++++++++++++++++++++++++++++++++++++
+//+				Comment Routes				 +
+//++++++++++++++++++++++++++++++++++++++++++++
+
+
+//Authorization routes
+
+router.get("/register",function(req,res){
+	res.render("register");
+
+
+});
+
+//handle registration logic
+router.post("/register",function(req,res){
+	var newUser = new User({username: req.body.username});
+	User.register(newUser, req.body.password, function (err, user){
+		if (err){
+			req.flash("error", err.message);
+			return res.redirect("register"); //short circuits out of the function and redirects us to register page
+		} else{
+			passport.authenticate("local")(req, res, function (){
+				req.flash("success", "Successfully registered " + user.username);
+				res.redirect("/homepage");
+
+
+			});
+		}
+	});
+
+
+});
+
+//showing login page
+router.get("/login",function(req,res){
+	res.render("login");
+
+
+});
+
+router.post("/login",passport.authenticate("local",
+		{
+			successRedirect: "/homepage",
+			failureRedirect: "/login"
+
+		}
+
+	) ,function(req,res){
+
+});
+
+
+
+//logout from navbar
+router.get("/logout", function(req,res){
+	{
+		req.logout();
+		req.flash("success", "Logged you out!");
+		res.redirect("/homepage");
+
+	}
+
+
+});
+
+
+module.exports = router;
